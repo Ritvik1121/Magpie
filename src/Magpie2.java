edcbb357e2c278cbb458b3f1b2b9d1a120fd9b3d
Ritvik <rintu1121@outlook.com>
2018-11-06 16:56:30 -0700

    Work on activity2

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -8,14 +8,14 @@
  * Handles responding to simple words and phrases</li>
  * </ul>
  * This version uses a nested if to handle default responses.
- * 
+ *
  * @author Laurie White
  * @version April 2012
  */
 public class Magpie2 {
 	/**
 	 * Get a default greeting
-	 * 
+	 *
 	 * @return a greeting
 	 */
 	public String getGreeting() {
@@ -24,20 +24,27 @@ public class Magpie2 {
 
 	/**
 	 * Gives a response to a user statement
-	 * 
+	 *
 	 * @param statement
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		String trimmed = statement.trim();
+		if (trimmed.length() == 0){
+			response = "Say something, please.";
+		} else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("dog") >= 0 || statement.indexOf("cat") >= 0){
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("Mr. Padjent") >= 0){
+			response = "Sounds like great teacher.";
 		} else {
 			response = getRandomResponse();
 		}
@@ -46,11 +53,11 @@ public class Magpie2 {
 
 	/**
 	 * Pick a default response to use if nothing else fits.
-	 * 
+	 *
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +70,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4){
+			response = "wow";
+		} else if (whichResponse == 5){
+			response = "Are you sure about that";
 		}
 
 		return response;
